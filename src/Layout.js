46a30d69b2cc58d416617be32d1bff07f557fe9c
Krzysztof Dziezyk <krzysztof.dziezyk@gmail.com>
2017-07-02 16:07:24 +0200

    initial Layout

---
+++ src/Layout.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default class Layout extends React.Component {
+
+	constructor() {
+		super();
+	}	
+
+
+	render() {
+		
+		return (
+			<div>
+				hello world! from - React JS
+			</div>
+		);
+	}
+}
