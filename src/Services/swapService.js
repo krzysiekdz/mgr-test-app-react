6e42dc0cd137e7445d353ea82caf5f2687318b4f
Krzysztof Dziezyk <krzysztof.dziezyk@gmail.com>
2017-07-05 17:43:54 +0200

    swap-service: created

---
+++ src/Services/swapService.js
@@ -0,0 +1,46 @@
+import ModelService from './modelService';
+import RandomService from './randomService';
+import ParseValueService from './parseValueService';
+
+export default class SwapService {
+
+	constructor() {
+		this.model = new ModelService().getModel();
+	}
+
+	swapFirst() {
+		var data = this.model.data;
+		if(data.length > 1) {
+			var i = 0;
+			var j = 1;
+			this.swap(i, j);
+		}
+	}
+
+	swapMid() {
+		var data = this.model.data;
+		if(data.length > 1) {
+			var i = Math.floor(data.length / 2) - 1;
+			var j = i+1;
+			this.swap(i, j);
+		}
+	}
+
+	swapLast() {
+		var data = this.model.data;
+		if(data.length > 1) {
+			var i = data.length-2;
+			var j = i+1;
+			this.swap(i, j);
+		}
+	}
+
+	swap(i, j) {
+		var data = this.model.data;
+
+		var temp = data[i];
+		this.model.data[i] = data[j];
+		this.model.data[j] = temp;
+	}		
+
+}
